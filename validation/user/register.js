9624f435c864ee96a2ff2d8eae973e612872a8b2
David <dhale12@hotmail.co.uk>
2019-03-30 08:35:20 +0000

    fix register validation

--- validation/user/register.js
+++ validation/user/register.js
@@ -18,20 +18,20 @@ module.exports = function validateRegisterInput(data) {
     errors.name = 'Name is required';
   }
 
-  if (Validator.isEmpty(email)) {
-    errors.email = 'Email is required';
-  }
-
   if (!Validator.isEmail(email)) {
     errors.email = 'Email is invalid';
   }
 
-  if (Validator.isEmpty(password)) {
-    errors.password = 'Password is required';
+  if (Validator.isEmpty(email)) {
+    errors.email = 'Email is required';
   }
 
   if (!Validator.isLength(password, { min: 6, max: 30 })) {
     errors.password = 'Password must be between 6 and 30 characters';
+  }
+
+  if (Validator.isEmpty(password)) {
+    errors.password = 'Password is required';
   }
 
   if (Validator.isEmpty(password2)) {
